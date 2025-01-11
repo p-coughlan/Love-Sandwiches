cc24ef25db8ddec87981ff742f415fb8aa02fc77
p-coughlan <patrickcoughlan@yahoo.co.uk>
2025-01-11 20:27:53 +0000

    Modify validate_data function to convert user string input into integer using list comprehension

--- run.py
+++ run.py
@@ -39,7 +39,9 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
+    print(values)
     try: # Try block will run the code inside it if it is successful
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
